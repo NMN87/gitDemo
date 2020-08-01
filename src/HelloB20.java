9010a7f6d749369eb378a491fbd914114c03e76e
NMN87 <milos.nikolic87@yahoo.com>
2020-08-01 10:02:41 -0400

    add Login.java

--- src/HelloB20.java
+++ src/HelloB20.java
@@ -3,7 +3,7 @@ public class HelloB20 {
     public static void main(String[] args) {
 
 
-        System.out.println(" Heloo B20, it is my pleasure");
+        System.out.println(" Hello B20, it is my pleasure");
 
 
 
